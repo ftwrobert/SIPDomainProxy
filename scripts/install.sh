b7f97b4a8f9d24cea97a31d5c69972126b17d580
Robert S Johnson <robert.j@bendtel.com>
2019-04-17 10:58:37 -0700

    Working Config

--- scripts/install.sh
+++ scripts/install.sh
@@ -32,7 +32,7 @@ SIPPORT='5060'
 SIPTLSPORT='5061'
 RTPE_MIN_PORT='52000'
 RTPE_MAX_PORT='54024'
-RTPE_SOCKET='127.0.0.1:2223'
+RTPE_SOCKET='udp:127.0.0.1:2223'
                   # Assuming we're installing on a LXD host
                   # The xt_RTPENGINE kernel module should already be loaded.
 RTPE_KERNEL_MOD=0 # Set to 1 if installing in a VM or a dedicated host.
