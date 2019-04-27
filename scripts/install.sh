e7569ef9d6f6fd8653141f20b020bb7ffa1ff032
Robert S Johnson <robert.j@bendtel.com>
2019-04-26 17:22:37 -0700

    Bug fixes

--- scripts/install.sh
+++ scripts/install.sh
@@ -44,7 +44,7 @@ SRCDIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && cd ../ && pwd )
 SRCKAMDIR="$SRCDIR/kamailio/conf"
 
 # create a symlink for proxyctl
-ln -s "$SRCDIR/scripts/proxyctl /usr/local/sbin/proxyctl"
+ln -s "$SRCDIR/scripts/proxyctl" "/usr/local/sbin/proxyctl"
 
 # Create user accounts
 if ! grep -qE "^/usr/sbin/nologin" /etc/shells
